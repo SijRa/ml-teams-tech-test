4bf8f1e1f25fe56e88344ca926eff2e690bb427c
Sijan Rana <sijan.rana@quantiphi.com>
2024-04-17 21:55:44 +0100

    add OperatorTable for operator related functionality

---
+++ src/ExtractCSV/operator_table.py
@@ -0,0 +1,55 @@
+import sys
+import logging
+
+from math import floor
+
+logger = logging.getLogger(__name__)
+
+class OperatorsTable:
+    """
+    Dictionary containing operators (e.g. O2, Three) and their respective prefix range
+    """
+
+    def __init__(self, operators_json) -> None:
+        """
+        operators.json required to instantiate the OperatorTable class
+        """
+        self.__table = self.__build(operators_json)
+        logger.info('OperatorTable built successfully')
+
+    def __build(self, operators_json) -> dict[int, str]:
+        """
+        Build operators table by enumerating through given operators json
+        """
+        table = {}
+        for index, operator in enumerate(operators_json):
+            operator = operators_json[index]['attributes']['operator']
+            prefix = operators_json[index]['attributes']['prefix']
+            table[int(prefix)] = operator
+        logger.debug('Operator table successfully built')
+        return table
+    
+    def find_operator(self, number_band:int) -> str:
+        """
+        Returns operator if found otherwise returns 'Unknown'
+        """
+        prefix_group = floor(number_band/1000)*1000
+        try:
+            operator = self.__table[prefix_group]
+            logger.debug('Operator found for prefix: %s', prefix_group)
+        except KeyError:
+            logger.warn('Operator NOT found for prefix: %s', prefix_group)
+            return 'Unknown'
+        return operator
+    
+    def __sizeof__(self) -> int:
+        """
+        Prints size of operators table
+        """
+        print(len(self.__table.values()))
+
+    def __str__(self) -> str:
+        """
+        Prints operators table
+        """
+        print(self.__table)
